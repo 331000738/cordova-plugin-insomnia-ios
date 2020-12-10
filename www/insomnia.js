53f333129c5e4c11e79184def618ba105340a13d
331000738 <331000783@qq.com>
2020-12-10 10:22:34 +0800

    尝试剥离android失败，退回版本

--- www/insomnia.js
+++ www/insomnia.js
@@ -1,4 +1,3 @@
-
 var exec    = require('cordova/exec'),
     channel = require('cordova/channel'),
     _isIos = false;
@@ -10,19 +9,33 @@ module.exports = {
         if(_isIos) {
             cordova.exec(successCallback, errorCallback, 'Insomnia', 'enable', []);
         } else {
-        	errorCallback("iOS Only");
+          errorCallback("iOS Only");
         }
     },
     disable: function (successCallback, errorCallback) {
         if(_isIos) {
             cordova.exec(successCallback, errorCallback, 'Insomnia', 'disable', []);
         } else {
-        	errorCallback("iOS Only");
+          errorCallback("iOS Only");
         }
     },
     switchOnScreenAndForeground: function (successCallback, errorCallback, options) {
         if(_isIos) {
-            cordova.exec(successCallback, errorCallback, 'Insomnia', 'switchOnScreenAndForeground', [options]);        
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'switchOnScreenAndForeground', [options]);
+        } else {
+            errorCallback("iOS Only");
+        }
+    },
+    acquireWakeLock: function (successCallback, errorCallback, options) {
+        if(_isIos) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'acquireWakeLock', [options]);
+        } else {
+            errorCallback("iOS Only");
+        }
+    },
+    releaseWakeLock: function (successCallback, errorCallback, options) {
+        if(_isIos) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'releaseWakeLock', [options]);
         } else {
             errorCallback("iOS Only");
         }
